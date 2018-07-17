a752240770c634834f65b61d8fdd6e19995cdbae
An Xiaoqiang <an_x@worksap.co.jp>
2018-07-17 12:21:18 +0800

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-CLI_APP=note2
-echo "input app name"
-read CLI_APP
+CLI_APP=note
+#echo "input app name"
+#read CLI_APP
 echo "install note cli app..."
 curl https://raw.githubusercontent.com/acearth/note-cli/master/note > /tmp/note-cli-tmp
 chmod +x /tmp/note-cli-tmp
