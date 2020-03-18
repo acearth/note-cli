436bf129d82ca83138d53a866da7a4b73d66ca93
An Xiaoqiang <an_x@worksap.co.jp>
2020-03-18 17:48:20 +0800

    .

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ CLI_APP=note
 echo "install note cli app..."
 curl https://raw.githubusercontent.com/acearth/note-cli/master/note > /tmp/note-cli-tmp
 chmod +x /tmp/note-cli-tmp
-sudo mv /tmp/note-cli-tmp /usr/local/bin/${CLI_APP}
+default_path=/usr/local/bin
+read -p "Input install path for the app:(${default_path}) "  install_path
+install_path="${install_path:-${default_path}}"
+echo "Your note application ${CLI_APP} will be installed on ${install_path}"
+echo "sudo password will be prompted on your local operation"
+sudo mv /tmp/note-cli-tmp ${install_path}/${CLI_APP}
